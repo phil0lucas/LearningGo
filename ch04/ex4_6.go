8e342d8631765c7c1a0a36db1dbcd84211af5695
Phil Lucas <phil@LucasProgramming.co.uk>
2017-02-24 11:33:51 +0000

    LearningGo 24Feb17-1

--- ch04/ex4_6.go
+++ ch04/ex4_6.go
@@ -6,28 +6,29 @@ package main
 import (
 	"fmt"
 	"bytes"
+	"unicode"
 )
 
 func main() {
 	// This is a byte slice
 	x := []byte("aaa       aaa bbb    cc  bbb")
-	//fmt.Println(bytes.Trim(x, "\x00"))
+	fmt.Printf("Original slice: %s\n", x)
 	fmt.Printf("Changed slice: %s\n",compress(x))
 }
 
-func compress(s []byte) []byte{
+func compress(s []byte) string{
 	var buf bytes.Buffer
-	fmt.Printf("In function: %s\n",s)
+	//fmt.Printf("In function: %s\n",s)
 	ii := 0
 	var last byte
 	for _, v := range s {
-		fmt.Println(v)
-		if (string(v) == "") & (v == last) {
+		// fmt.Println(v)
+		if (unicode.IsSpace(rune(v))) && (v == last) {
 		} else {
-			buf.WriteString(v)
+			buf.WriteByte(v)
 		}
 		last = v
 		ii++
 	}
-	return string(buf)
+	return buf.String()
 }
