61e02f7db838c4af31fef67a0b4b31f1e7937280
Phil Lucas <phil@LucasProgramming.co.uk>
2017-02-28 10:10:37 +0000

    LearningGo 28Feb17-1

---
+++ ch04/movie.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type Movie struct {
+	Title  string
+	Year   int  `json:"released"`
+	Color  bool `json:"color,omitempty"`
+	Actors []string
+}
+
+var movies = []Movie{
+	{Title: "Casablanca", Year: 1942, Color: false,
+		Actors: []string{"Humphrey Bogart", "Ingrid Bergman"}},
+	{Title: "Cool Hand Luke", Year: 1967, Color: true,
+		Actors: []string{"Paul Newman"}},
+	{Title: "Bullitt", Year: 1968, Color: true,
+		Actors: []string{"Steve McQueen", "Jacqueline Bisset"}},
+}
+
+func main() {
+	//!+Marshal - conversion of a Go data structure to JSON
+	data, err := json.Marshal(movies)
+	if err != nil {
+		log.Fatalf("JSON marshaling failed: %s", err)
+	}
+	fmt.Printf("%s\n", data)
+	//!-Marshal
+	
+	// Marshal with indent for readability
+	data, err = json.MarshalIndent(movies, "", "    ")
+	if err != nil {
+		log.Fatalf("JSON marshaling failed: %s", err)
+	}
+	fmt.Printf("%s\n", data)
+	
+	
+	//!+Unmarshal - take the JSON and create a Go data structure
+	// Defines a slice of structs each having one field
+	var titles []struct{ Title string }
+	// Data is written to the address of the data structure
+	if err := json.Unmarshal(data, &titles); err != nil {
+		log.Fatalf("JSON unmarshaling failed: %s", err)
+	}
+	fmt.Println(titles) // "[{Casablanca} {Cool Hand Luke} {Bullitt}]"
+	//!-Unmarshal	
+}
