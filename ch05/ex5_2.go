e852f9f290687f029e4f0489d48714af8debd86c
Phil Lucas <phil@LucasProgramming.co.uk>
2017-03-15 11:06:45 +0000

    LearningGo 15Mar17-1

---
+++ ch05/ex5_2.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"golang.org/x/net/html"
+)
+
+func visit(n *html.Node) {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, a := range n.Attr {
+			if a.Key == "href" && strings.HasSuffix(a.Val, ".jpg"){
+				fmt.Println(a.Val)
+			}
+		}
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		visit(c)
+	}
+}
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlinks1: %v\n", err)
+		os.Exit(1)
+	}
+	visit(doc)
+}	
